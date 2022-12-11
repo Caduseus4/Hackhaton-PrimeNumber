c3e3d48a7b53e25df405d28801b9cb0a92ea4375
Sakir Ayitki <ayitkisakir1@gmail.com>
2022-12-11 03:26:49 +0300

    PrimeNumber

--- HackhatonPrimeNumber/src/HackhatonPrimeNumber.java
+++ HackhatonPrimeNumber/src/HackhatonPrimeNumber.java
@@ -7,21 +7,25 @@ public class HackhatonPrimeNumber {
         System.out.print("Enter A Number From 1 To 9:");
         int x = scanner.nextInt();
 
-        int counter = 0, counter1=0;
+        /* Tum asal sayilar icin ve istenen sayilar icin counterlar tanimladik.  */
+        int counter = 0, counter1 = 0;
+
+        /* Asal sayiyi bulma algoritmasi */
         for (int num = 2; num <= 50; num++) {
             int control = 0;
 
             for (int i = 2; i < num; i++) {
 
-                if (num%i==0){
+                if (num % i == 0) {
                     control = 1;
                     break;
                 }
             }
 
-            if (control==0){
-                if((num+5)%x==0){
-                    System.out.println(num+5);
+            /* Sayi asalsa if blogunun icine girer sayiyi 5 ile toplar ve x'e boler eger sayi x'in katiysa ekrana yazdirir. */
+            if (control == 0) {
+                if ((num + 5) % x == 0) {
+                    System.out.print((num + 5) + " ");
                     counter1++;
                 }
 
@@ -29,7 +33,8 @@ public class HackhatonPrimeNumber {
             }
 
         }
-        System.out.println("Number Of Prime Numbers Between 1-50: "+ counter);
+        System.out.println("\n");
+        System.out.println("Number Of Prime Numbers Between 1-50: " + counter);
         System.out.println("Number of Requested Number: " + counter1);
     }
 
